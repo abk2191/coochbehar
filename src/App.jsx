686d5cc0228e538d70b9fe53e3297dbaaf2070ad
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-14 12:48:41 +0530

    Added new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Navbar from "./Navbar";
 import Banner from "./Banner";
+import CityDescription from "./CityDescription";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <CityDescription />
     </>
   );
 }
---
+++ src/CityDescription.jsx
@@ -0,0 +1,20 @@
+function CityDescription() {
+  return (
+    <>
+      <div className="city-description">
+        <h2>COOCHBEHAR - THE ROYAL CITY</h2>
+        <p>
+          Coochbehar is a quiet heritage city in North Bengal, known for its
+          serene vibe and slow-paced lifestyle. The centerpiece is the stunning
+          Cooch Behar Palace, inspired by classical European architecture and
+          surrounded by lush gardens. The city has clean, wide roads, plenty of
+          ponds, and a calmness that makes everyday life feel simple and
+          predictable. It’s a place where tradition is strong, people are
+          polite, and time moves a little slower than the rest of the world.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default CityDescription;

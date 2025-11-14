9c01ffbdf1727d966ab97d7e50424ce184452e98
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-14 14:05:15 +0530

    Added new features

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,23 @@
 function Sidebar() {
+  const sidebarItems = [
+    "Home",
+    "Important Places",
+    "Places Nearby",
+    "Events",
+    "Photos",
+    "Contact",
+  ];
   return (
     <>
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <ul>
+          {sidebarItems.map((items) => (
+            <li key={items} className="sidebar-items">
+              <a href="">{items}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }

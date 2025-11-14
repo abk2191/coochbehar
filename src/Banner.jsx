a3a58b2c46e2991c32445dd51d0a8b9b35163506
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-14 12:40:06 +0530

    Added new features

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,11 +1,8 @@
 function Banner() {
   return (
-    <>
-      <div className="banner">
-        <img src="./cob.jpg" alt="Space background" className="banner-image" />
-      </div>
-      ;
-    </>
+    <div className="banner">
+      <img src="./cob.jpg" alt="cob image" className="banner-image" />
+    </div>
   );
 }
 

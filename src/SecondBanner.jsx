16e184e2fc4e183bfd4e81eb88e6418cb4588468
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-15 15:34:58 +0530

    Added new features

--- src/SecondBanner.jsx
+++ src/SecondBanner.jsx
@@ -8,18 +8,20 @@ function SecondBanner() {
           className="second-banner-image"
         />
       </div>
-      <div className="sagar-dighi-desc-div">
-        <h2>THE MAJESTIC SAGAR DIGHI</h2>
-        <p>
-          Sagar Dighi is one of the most prominent public spaces in Coochbehar,
-          known for its expansive waterbody and well-maintained surroundings.
-          Each evening, residents gather along its walkways, engaging in casual
-          conversations, leisurely strolls, and quiet relaxation by the
-          lakeside. The periphery becomes active with small food stalls offering
-          tea and local snacks, adding to the area’s welcoming atmosphere.
-          Together, these elements make Sagar Dighi an important social and
-          recreational hub within the city.
-        </p>
+      <div className="container">
+        <div className="sagar-dighi-desc-div">
+          <h2>THE MAJESTIC SAGAR DIGHI</h2>
+          <p>
+            Sagar Dighi is one of the most prominent public spaces in
+            Coochbehar, known for its expansive waterbody and well-maintained
+            surroundings. Each evening, residents gather along its walkways,
+            engaging in casual conversations, leisurely strolls, and quiet
+            relaxation by the lakeside. The periphery becomes active with small
+            food stalls offering tea and local snacks, adding to the area’s
+            welcoming atmosphere. Together, these elements make Sagar Dighi an
+            important social and recreational hub within the city.
+          </p>
+        </div>
       </div>
     </>
   );

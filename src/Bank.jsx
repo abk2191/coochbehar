8b1bf9010767828d69011f07b7ec85318a94b08a
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-17 13:55:41 +0530

    Added new features

--- src/Bank.jsx
+++ src/Bank.jsx
@@ -30,29 +30,52 @@ const Bank = ({ isOpen, onClose }) => {
             <p>Pincode: 736101</p>
           </div>
           <div className="bank-item">
-            <h3>Punjab National Bank</h3>
-            <p>J.N. Road, Coochbehar</p>
-            <p>Phone: 03582-345678</p>
+            <h3>INDIAN BANK – BISWA SINGHA ROAD BRANCH</h3>
+            <p>
+              Biswa Singh Road,Oppistie Madan Mohan Temple,Cooch Behar-I
+              Block,Cooch Behar, West Bengal
+            </p>
+            <p>Email : br[dot]coochbehar_bsroad[at]allahabadbank[dot]in</p>
+            <p>Phone : 919678129219</p>
+            <p>Website Link : https://www.indianbank.net.in</p>
+            <p>Pincode: 736101</p>
           </div>
           <div className="bank-item">
-            <h3>Bank of India</h3>
-            <p>B.S. Road, Coochbehar</p>
-            <p>Phone: 03582-456789</p>
+            <h3>PUNJAB NATIONAL BANK – COOCHBEHAR BRANCH</h3>
+            <p>Sunity Road,Cooch Behar-I Block,Cooch Behar, West Bengal</p>
+            <p>Email : bo024020[at]pnb[dot]co[dot]in</p>
+            <p>Phone : 919002152844</p>
+            <p>Website Link : https://www.pnbindia.in</p>
+            <p>Pincode: 736101</p>
           </div>
           <div className="bank-item">
-            <h3>State Bank of India</h3>
-            <p>Main Branch, Coochbehar</p>
-            <p>Phone: 03582-123456</p>
+            <h3>SBI – COOCHBEHAR BAZAR BRANCH</h3>
+            <p>
+              Nripendra Narayan Road,Cooch Behar-I Block,Cooch Behar, West
+              Bengal
+            </p>
+            <p>Email : sbi[dot]01753[at]sbi[dot]co[dot]in</p>
+            <p>Phone : 918001196073</p>
+            <p>Website Link : https://www.sbi.co.in</p>
+            <p>Pincode: 736101</p>
           </div>
           <div className="bank-item">
-            <h3>State Bank of India</h3>
-            <p>Main Branch, Coochbehar</p>
-            <p>Phone: 03582-123456</p>
+            <h3>SBI – COOCHBEHAR MAIN BRANCH</h3>
+            <p>
+              Sagardighi Square,Cooch Behar-I Block,Cooch Behar, West Bengal
+            </p>
+            <p>Email : sbi[dot]00058[at]sbi[dot]co[dot]in</p>
+            <p>Phone : 918001196024</p>
+            <p>Website Link : https://www.sbi.co.in</p>
+            <p>Pincode: 736101</p>
           </div>
           <div className="bank-item">
-            <h3>State Bank of India</h3>
-            <p>Main Branch, Coochbehar</p>
-            <p>Phone: 03582-123456</p>
+            <h3>SBI – KHAGRABARI ADB BRANCH</h3>
+            <p>Khagrabari,Cooch Behar-II Block,Cooch Behar, West Bengal</p>
+            <p>Email : sbi[dot]04785[at]sbi[dot]co[dot]in</p>
+            <p>Phone : 918001196095</p>
+            <p>Website Link : https://www.sbi.co.in</p>
+            <p>Pincode: 736101</p>
           </div>
         </div>
         <div className="centered">
